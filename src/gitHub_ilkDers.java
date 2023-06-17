78dead35d1f04cd0da28b94715784033a6d2c987
trtkyunus <tartikyunus656@gmail.com>
2023-06-17 12:50:19 +0200

    170623

--- src/gitHub_ilkDers.java
+++ src/gitHub_ilkDers.java
@@ -2,6 +2,8 @@ public class gitHub_ilkDers {
 
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
+        System.out.println("=====================");
+        System.out.println("have a wise day");
 
     }
 }
